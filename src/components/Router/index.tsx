6e11acf6cf80d401c32f2b1c1987402f6a4a5a6b
Gor Ayvazyan <gor.ayvazyan.work@gmail.com>
2023-08-02 17:01:03 +0400

    Change name

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -1,12 +1,12 @@
 import React, { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-const Chat = lazy(() => import(/* webpackChunkName: "Cats" */ "pages/Main"));
+const Main = lazy(() => import(/* webpackChunkName: "Cats" */ "pages/Main"));
 const Router = () => {
   return (
     <>
       <Suspense fallback={<p>Loading ....</p>}>
         <Routes>
-          <Route path="/" element={<Chat />} />
+          <Route path="/" element={<Main />} />
         </Routes>
       </Suspense>
     </>
